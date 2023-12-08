5562aa7176efe371b1d559fffe81d13ef1f7e71c
Alexander Elias <alex.steven.elias@gmail.com>
2023-12-08 13:08:23 -0800

    updated deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
-export { encode } from 'https://deno.land/std@0.184.0/encoding/base64.ts';
-export { typeByExtension } from 'https://deno.land/std@0.184.0/media_types/mod.ts';
+export { encodeBase64 } from 'https://deno.land/std@0.208.0/encoding/base64.ts';
+export { contentType } from 'https://deno.land/std@0.208.0/media_types/content_type.ts';
+export { extname } from 'https://deno.land/std@0.208.0/path/mod.ts';
